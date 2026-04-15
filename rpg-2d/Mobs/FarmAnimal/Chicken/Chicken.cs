0a7de68e0b8cf36450221fdfefbaa0b085d91dd2
Jakob <jakob.roth@stud.hs-flensburg.de>
2026-04-16 00:36:18 +0200

    Assets struckture and add colors

--- rpg-2d/Mobs/FarmAnimal/Chicken/Chicken.cs
+++ rpg-2d/Mobs/FarmAnimal/Chicken/Chicken.cs
@@ -2,24 +2,24 @@ using Godot;
 
 public partial class Chicken : MobBase
 {
-    private AnimatedSprite2D _sprite;
-    private string _dir = "down";
+	private AnimatedSprite2D _sprite;
+	private string _dir = "down";
 
-    protected override void OnReady()
-    {
-        _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-        PlayAnim("idle");
-    }
+	protected override void OnReady()
+	{
+		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		PlayAnim("idle");
+	}
 
-    private void PlayAnim(string anim)
-    {
-        string full = anim + "_" + _dir;
-        if (_sprite.SpriteFrames.HasAnimation(full))
-            _sprite.Play(full);
-        else if (_sprite.SpriteFrames.HasAnimation(anim + "_down"))
-            _sprite.Play(anim + "_down");
-    }
+	private void PlayAnim(string anim)
+	{
+		string full = anim + "_" + _dir;
+		if (_sprite.SpriteFrames.HasAnimation(full))
+			_sprite.Play(full);
+		else if (_sprite.SpriteFrames.HasAnimation(anim + "_down"))
+			_sprite.Play(anim + "_down");
+	}
 
-    public override void TakeDamage(float amount) { base.TakeDamage(amount); }
-    protected override void Die() { base.Die(); }
+	public override void TakeDamage(float amount) { base.TakeDamage(amount); }
+	protected override void Die() { base.Die(); }
 }
